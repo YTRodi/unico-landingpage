4b3e535e034f3117458b4cfaff5c6e3b75a46b66
YTRodi <yagorodi@gmail.com>
2022-12-04 11:33:39 -0300

    feat: fonts and colors config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-import reactLogo from './assets/react.svg';
-
 function App() {
   const [count, setCount] = useState(0);
 
@@ -11,11 +9,8 @@ function App() {
         <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
           <img alt="Vite logo" className="logo" src="/vite.svg" />
         </a>
-        <a href="https://reactjs.org" rel="noreferrer" target="_blank">
-          <img alt="React logo" className="logo react" src={reactLogo} />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="font-gilroy text-pink">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      pink: {
+        DEFAULT: '#FB64B0',
+        light: '#FAE3EF',
+      },
+      purple: {
+        DEFAULT: '#6455EC',
+        light: '#E8D7FF',
+      },
+      yellow: {
+        DEFAULT: '#FFAC32',
+        light: '#FBF3CA',
+      },
+      gray: {
+        DEFAULT: '#777B84',
+        light: '#4B4B4B',
+        dark: '#323336',
+      },
+    },
+    extend: {
+      fontFamily: {
+        gilroy: ['Gilroy'],
+      },
+    },
   },
   plugins: [],
 };
